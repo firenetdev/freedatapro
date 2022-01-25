a2a221c055073163e46f0b88295ebbf16d10fca5
Firenet Philippines <33688702+firenetdev@users.noreply.github.com>
2022-01-25 19:29:16 +0800

    Update debian_ovpnws.sh

--- debian_ovpnws.sh
+++ debian_ovpnws.sh
@@ -674,21 +674,21 @@ sysctl -p
 
 install_rclocal(){
   {
-    wget https://pastebin.com/raw/faSsYhFf -O /etc/ubuntu
-    dos2unix /etc/ubuntu
-    chmod +x /etc/ubuntu    
-    screen -dmS socks python /etc/ubuntu
+    wget --no-check-certificate https://pastebin.com/raw/faSsYhFf -O /etc/socks.py
+    dos2unix /etc/socks.py
+    chmod +x /etc/socks.py
+    screen -dmS socks python /etc/socks.py
     wget --no-check-certificate https://pastebin.com/raw/658HpnLd -O /etc/systemd/system/rc-local.service
     echo "#!/bin/sh -e
-iptables-restore < /etc/iptables_rules.v4
-ip6tables-restore < /etc/iptables_rules.v6
-sysctl -p
-service squid3 restart
-service stunnel4 restart
-service openvpn@server restart
-service openvpn@server2 restart
-screen -dmS socks python /etc/ubuntu
-exit 0" >> /etc/rc.local
+    iptables-restore < /etc/iptables_rules.v4
+    ip6tables-restore < /etc/iptables_rules.v6
+    sysctl -p
+    service squid3 restart
+    service stunnel4 restart
+    service openvpn@server restart
+    service openvpn@server2 restart
+    screen -dmS socks python /etc/ubuntu
+    exit 0" >> /etc/rc.local
     sudo chmod +x /etc/rc.local
     sudo systemctl enable rc-local
     sudo systemctl start rc-local.service
